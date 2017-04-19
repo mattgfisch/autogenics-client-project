8edcb78b0ee0e47fa0ed721363bd3d88b29d58c1
Joshua Mun <email": null>
2017-04-19 16:30:31 -0700

    Add migrations and models

---
+++ app/models/experiment_staff.rb
@@ -0,0 +1,2 @@
+class ExperimentStaff < ApplicationRecord
+end
---
+++ db/migrate/20170419231955_create_experiments.rb
@@ -0,0 +1,11 @@
+class CreateExperiments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :experiments do |t|
+      t.string :title, null: false
+      t.text :content, null: false
+      t.integer :author_id, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
