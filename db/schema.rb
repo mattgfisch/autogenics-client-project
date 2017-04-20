ceb82ced152e623c8700d730ae94876d65500d15
Linsey R <email": null>
2017-04-20 10:09:51 -0700

    edit seed data to reflect most recent migration for experiments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420040929) do
+ActiveRecord::Schema.define(version: 20170420170729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170420040929) do
     t.text     "acknowledgments"
     t.text     "references"
     t.text     "author_contributions"
+    t.integer  "staff_size"
   end
 
   create_table "experiments_staff", force: :cascade do |t|
