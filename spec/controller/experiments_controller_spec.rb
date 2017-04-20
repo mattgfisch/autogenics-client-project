729be1d140ec8cbb70d06597d33640763ecea76c
Linsey R <email": null>
2017-04-20 12:18:30 -0700

    Edit tests for experiment controller

--- spec/controller/experiments_controller_spec.rb
+++ spec/controller/experiments_controller_spec.rb
@@ -1,1 +1,48 @@
-require 'rais'
+require 'rails_helper'
+
+describe ExperimentsController do
+  let!(:faculty) { Role.create!(name: "Faculty") }
+  let!(:josh) { User.create!(name: "Josh", email: "email@email.com", password: "password", role_id: faculty.id) }
+  let!(:experiment) { Experiment.create!( title: Faker::Lorem.sentence,
+                    abstract: Faker::Lorem.paragraph,
+                    introduction: Faker::Lorem.paragraph,
+                    materials: Faker::Lorem.paragraph,
+                    results: Faker::Lorem.paragraph,
+                    discussion: Faker::Lorem.paragraph,
+                    conclusion: Faker::Lorem.paragraph,
+                    supporting_info: Faker::Lorem.paragraph,
+                    acknowledgments: Faker::Lorem.paragraph,
+                    references: Faker::Lorem.paragraph,
+                    author_contributions: Faker::Lorem.paragraph,
+                    author_id: josh.id) }
+
+  describe 'GET #show' do
+    it 'responds with a status code 200' do
+      get :show, { id: exp.id }
+      expect(response). to have_http_status 200
+    end
+  end
+  describe "GET #new" do
+    before(:each) do
+      get :new
+    end
+
+    it 'responds with a status code 200' do
+      expect(response.status).to eq 200
+    end
+
+    it 'renders the :new template for an experiment' do
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'POST #create' do
+    before(:each) do
+     post :create,  experiment: exp
+   end
+
+   it 'responds with a status code of 202' do
+    expect(response.status).to eq 202
+  end
+  end
+end
