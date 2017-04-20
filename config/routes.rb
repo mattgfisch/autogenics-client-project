2dfe4e9141b33abb32bb5b9f6b7045c789c28529
Philip Cornman <pjc5108@gmail.com>
2017-04-20 11:14:30 -0700

    nav bar links are up, routes defined, methods still need to be created in controller.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
 
+  get 'users/login'
+  post 'users/login'
+  delete 'users/logout'
 
   root to: 'experiments#index'
 
