4b4739c18ac18cd20a3c3b51ac1d9e950a994da4
Linsey R <email": null>
2017-04-20 13:16:36 -0700

    add minimal styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root to: 'experiments#index'
+  get 'users/login', :to => 'sessions#new'
+  post 'users/login'
+  delete 'users/logout'
+
 
   resources :users
 
-  get 'users/login'
-  post 'users/login'
-  delete 'users/logout'
+
 
   resources :experiments
   resources :sessions, only: [:index, :new, :create, :delete]
