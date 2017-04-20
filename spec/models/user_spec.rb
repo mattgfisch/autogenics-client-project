df846680f6cf08b584847dcab5fc5cca4da8a4af
joshmun <joshuamun@gmail.com>
2017-04-19 22:38:08 -0700

    refactored model association tests using before each and let statements

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,57 +3,54 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   describe "associations" do
+    
     let (:josh) { User.new(name: "Josh", email: "email@email.com", password: "password") }
     let(:austin) { User.new(name: "Austin", email: "email@email.com", password: "password") }
-    let (:peas) { Experiment.create!(title: "Peas and Carrots", content: "Tattoos are cool") }
+    before(:each) do
+      josh.role = Role.create!(name: "Faculty")
+      josh.save
+    end
+
     context 'roles' do
-
-
       it 'has a role as faculty' do
-        josh.role = Role.create!(name: "Faculty")
-        josh.save
         expect(josh.clearance_levels).to eq "Faculty"
       end
 
       it 'has a role as staff' do
-        austin.role = Role.create!(name: "Faculty")
-        austin.save
+          austin.role = Role.create!(name: "Faculty")
+          austin.save
         expect(austin.clearance_levels).to eq "Faculty"
       end
     end
 
+    before(:each) do
+      austin.role = Role.create!(name: "Staff")
+      austin.save
+    end
+
+    let (:experiment) { Experiment.create!(title: "Study in Scarlet", content: "Sherlock Holmes", author_id: josh.id) }
+
     context 'user and experiments associations' do
       it "author has many experiments posted" do
-        josh.role = Role.create!(name: "Faculty")
-        josh.save
-        Experiment.create!(title: "Study in Scarlet", content: "Sherlock Holmes", author_id: josh.id)
+        experiment
         expect(josh.experiments_posted.count).to eq 1
       end
 
       it "experiments have one author" do
-        josh.role = Role.create!(name: "Faculty")
-        josh.save
-        experiment = Experiment.create!(title: "Study in Scarlet", content: "Sherlock Holmes", author_id: josh.id)
         expect(experiment.author).to eq josh
       end
 
       it "experiments have many staff" do
-        austin.role = Role.create!(name: "Staff")
-        austin.save
-        experiment = Experiment.create!(title: "Study in Scarlet", content: "Sherlock Holmes", author_id: austin.id)
+        experiment.author_id = austin.id
         experiment.staff << austin
         expect(experiment.staff.first).to eq austin
       end
 
       it "staff have many experiments" do
-        austin.role = Role.create!(name: "Staff")
-        austin.save
-        experiment = Experiment.create!(title: "Study in Scarlet", content: "Sherlock Holmes", author_id: austin.id)
+        experiment.author_id = austin.id
         experiment.staff << austin
         expect(austin.experiments_joined.first).to eq experiment
       end
-
-
     end
   end
 end
