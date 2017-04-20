1950ef77c486d36a6673afd7bebac417d3e80c58
Austin Kading <email": null>
2017-04-19 21:52:37 -0700

    Write tests for user roles and associations with experiments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 Role.destroy_all
-Role.create!(name: "Faculty")
-Role.create!(name: "Staff")
+ faculty = Role.create!(name: "Faculty")
+staff = Role.create!(name: "Staff")
 
-User.create!(name: "Josh", email: "email@email.com", password: "password", role_id: 1)
-User.create!(name: "Austin", email: "email@email.com", password: "password", role_id: 2)
+User.create!(name: "Josh", email: "email@email.com", password: "password", role_id: faculty.id)
+User.create!(name: "Austin", email: "email@email.com", password: "password", role_id: staff.id)
 
 Experiment.create!(title: "Study in Scarlet", content: "Sherlock Holmes", author_id: 1)
 Experiment.create!(title: "Peas and Carrots", content: "Tattoos are cool", author_id: 1)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,30 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-      let (:faculty ) { Role.create!(name: "Faculty") }
-      let (:staff) { Role.create!(name: "Staff") }
-      let (:josh) {User.create!(name: "Josh", email: "email@email.com", password: "password", role_id: 1)}
-      let (:austin) {User.create!(name: "Austin", email: "email@email.com", password: "password", role_id: 2)}
-      let (:sherlock) { Experiment.create!(title: "Study in Scarlet", content: "Sherlock Holmes", author_id: josh.id) }
-      let (:peas) { Experiment.create!(title: "Peas and Carrots", content: "Tattoos are cool", author_id: josh.id) }
-
 
   describe "associations" do
+    let (:josh) { User.new(name: "Josh", email: "email@email.com", password: "password") }
+    let(:austin) { User.new(name: "Austin", email: "email@email.com", password: "password") }
+    let (:peas) { Experiment.create!(title: "Peas and Carrots", content: "Tattoos are cool") }
     context 'roles' do
+
+
       it 'has a role as faculty' do
+        josh.role = Role.create!(name: "Faculty")
+        josh.save
         expect(josh.clearance_levels).to eq "Faculty"
       end
 
       it 'has a role as staff' do
-        expect(austin.clearance_levels).to eq "Staff"
+        austin.role = Role.create!(name: "Faculty")
+        austin.save
+        expect(austin.clearance_levels).to eq "Faculty"
       end
     end
 
     context 'user and experiments associations' do
-      xit "user has many experiments" do
-        # p josh
-        expect(josh.experiments.count).to eq 2
+      it "author has many experiments posted" do
+        josh.role = Role.create!(name: "Faculty")
+        josh.save
+        Experiment.create!(title: "Study in Scarlet", content: "Sherlock Holmes", author_id: josh.id)
+        expect(josh.experiments_posted.count).to eq 1
       end
+
+      it "experiments have one author" do
+        josh.role = Role.create!(name: "Faculty")
+        josh.save
+        experiment = Experiment.create!(title: "Study in Scarlet", content: "Sherlock Holmes", author_id: josh.id)
+        expect(experiment.author).to eq josh
+      end
+
+      it "experiments have many staff" do
+        austin.role = Role.create!(name: "Staff")
+        austin.save
+        experiment = Experiment.create!(title: "Study in Scarlet", content: "Sherlock Holmes", author_id: austin.id)
+        experiment.staff << austin
+        expect(experiment.staff.first).to eq austin
+      end
+
+      it "staff have many experiments" do
+        austin.role = Role.create!(name: "Staff")
+        austin.save
+        experiment = Experiment.create!(title: "Study in Scarlet", content: "Sherlock Holmes", author_id: austin.id)
+        experiment.staff << austin
+        expect(austin.experiments_joined.first).to eq experiment
+      end
+
+
     end
   end
 end
