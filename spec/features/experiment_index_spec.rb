820d433e92aa6c9bcc143329d4d26456dd8084e5
Philip Cornman <pjc5108@gmail.com>
2017-04-20 12:02:40 -0700

    changed second test to pending.

--- spec/features/experiment_index_spec.rb
+++ spec/features/experiment_index_spec.rb
@@ -22,21 +22,6 @@ describe 'Experiment' do
     end
 
     xit "has a clickable 'Post New Experiment' button" do
-      faculty = Role.create!(name: "Faculty")
-      linsey = User.create!(name: "Josh", email: "email@email.com", password: "password", role_id: faculty.id)
-      new_exp = Experiment.create!( title: Faker::Lorem.sentence,
-                    abstract: Faker::Lorem.paragraph,
-                    introduction: Faker::Lorem.paragraph,
-                    materials: Faker::Lorem.paragraph,
-                    results: Faker::Lorem.paragraph,
-                    discussion: Faker::Lorem.paragraph,
-                    conclusion: Faker::Lorem.paragraph,
-                    supporting_info: Faker::Lorem.paragraph,
-                    acknowledgments: Faker::Lorem.paragraph,
-                    references: Faker::Lorem.paragraph,
-                    author_contributions: Faker::Lorem.paragraph,
-                    author_id: linsey.id)
-
       visit "/experiments"
       click_on "Post New Experiment"
       expect(page).to have_current_path new_experiment_path
