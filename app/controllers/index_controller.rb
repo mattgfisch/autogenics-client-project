223c1df7dff6a026718a16dbba5e386dc0bd4f17
Linsey R <email": null>
2017-04-21 12:13:52 -0700

    refactor code

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,10 +1,11 @@
 class IndexController < ApplicationController
 
   def index
+    @experiments = Experiment.order(created_at: :desc)
     @user = User.new
     if request.xhr?
       if session[:user_id]
-        @experiments = Experiment.all
+        @experiments = Experiment.order(created_at: :desc)
         render partial: "experiments/index"
         else
         render partial: "sessions/index"
