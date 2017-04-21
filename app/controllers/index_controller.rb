596be86b117f23cc008ace3575db8552b90ba5fc
Matthew Fischer <email": null>
2017-04-21 15:45:17 -0700

    Commit before test

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -4,13 +4,19 @@ class IndexController < ApplicationController
     @experiments = Experiment.order(created_at: :desc)
     @user = User.new
     if request.xhr?
+      respond_to do |format|
+        format.js {render action: 'navigation_show' }
+      end
       if session[:user_id]
         @experiments = Experiment.order(created_at: :desc)
         render partial: "experiments/index"
-        else
+      else
         render partial: "sessions/index"
       end
     end
+  end
+
+  def navigation_show
   end
 
 end
