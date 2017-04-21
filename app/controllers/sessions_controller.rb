4ee957e0c8b96b4eb886127f6208c6aaa06bd7b2
Philip Cornman <pjc5108@gmail.com>
2017-04-21 16:25:13 -0700

    show errors if user enters wrong login or registration information.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,15 +7,14 @@ class SessionsController < ApplicationController
     respond_to do |format|
       format.js {}
     end
-
   end
 
   def create
-    user = User.find_by_email(user_params[:email])
+    @user = User.find_by_email(user_params[:email])
     @experiments = Experiment.all
-    if user && user.authenticate(user_params[:password])
-      session[:user_id] = user.id
-      session[:role] = user.clearance_levels
+    if @user && @user.authenticate(user_params[:password])
+      session[:user_id] = @user.id
+      session[:role] = @user.clearance_levels
       if request.xhr?
         respond_to do |format|
           format.js {}
@@ -25,6 +24,8 @@ class SessionsController < ApplicationController
       end
     else
       if request.xhr?
+        @user = User.new unless @user
+        @message = "Incorrect email or password."
         respond_to do |format|
           format.js { render action: 'new' }
         end
