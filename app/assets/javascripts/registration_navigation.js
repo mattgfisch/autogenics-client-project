80ecdce538b05d6ebe65052bef88a6f9c246162e
Matthew Fischer <email": null>
2017-04-20 18:55:00 -0700

    Create registration javascript file

---
+++ app/assets/javascripts/registration_navigation.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $('#button-to-registration').on('click', function(event){
+    event.preventDefault();
+    var button_path = $(this).parent().attr('action');
+    var button_method = $(this).parent().attr('method');
+
+    var request = $.ajax({
+      url: button_path,
+      type: button_method
+    });
+    request.done(function(response){
+
+    });
+  });
+});
